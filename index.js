62501e45d174ecf3f24680d8dc36b5bbe5d17fe6
Michael <michaelj.silva@hotmail.com>
2024-01-23 23:06:39 +0000

    Defined functions to get info for each team role

--- index.js
+++ index.js
@@ -13,3 +13,97 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const team = [];
+
+function managerInfo() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Enter manager's name:",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Enter manager's employee ID:",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Enter manager's email:",
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "Enter manager's office number:",
+            },
+        ])
+        .then((answers) => {
+            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+            team.push(manager);
+            promptGetTeam();
+        });
+}
+
+function engineerInfo() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Enter engineer's name:",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Enter engineer's employee ID:",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Enter engineer's email:",
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "Enter engineer's GitHub username:",
+            },
+        ])
+        .then((answers) => {
+            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            team.push(engineer);
+            promptGetTeam();
+        });
+}
+
+function internInfo() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Enter intern's name:",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Enter intern's employee ID:",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Enter intern's email:",
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "Enter intern's school:",
+            },
+        ])
+        .then((answers) => {
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+            team.push(intern);
+            promptGetTeam();
+        });
+    }
